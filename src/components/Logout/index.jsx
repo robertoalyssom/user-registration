3fd1d2c832cc3de9f5688f4e439cd4fdf1fc5485
robertoalyssom <robert_alys@hotmail.com>
2025-09-19 12:25:26 -0300

    feat: add delete account functionality

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,6 +1,7 @@
 import "./style.css";
 import useAuthContext from "../../context/AuthContext/useAuthContext.js";
 import logoutUser from "../../services/logout.js";
+import deleteUser from "../../services/deleteUser.js";
 
 export default function Logout() {
   const { logout } = useAuthContext();
@@ -10,8 +11,16 @@ export default function Logout() {
     logout();
   }
 
+  async function handleDeleteAccount() {
+    const data = await deleteUser();
+    data.message && logout();
+  }
+
   return (
     <div className="logout-ctn">
+      <button className="delete-account_btn" onClick={handleDeleteAccount}>
+        Delete account
+      </button>
       <p>Log out</p>
       <button className="logout-btn" onClick={handleLogout}>
         <i className="fa-solid fa-arrow-right-from-bracket"></i>
---
+++ src/services/deleteUser.js
@@ -0,0 +1,14 @@
+import api from "./api.js";
+
+export default async function deleteUser() {
+  console.log("Deleting user...");
+  try {
+    const response = await api.delete("/auth/deleteAccount");
+    console.log(" response", response);
+
+    console.log("User deleted successfully");
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting user:", error);
+  }
+}
